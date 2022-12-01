6665d6e31ac458f3ed32b127158d5667e9a62d0d
Kiran B A <kiranbanjagaric@gmail.com>
2022-12-01 20:26:09 +0530

    Init and Integrate Single Sign On

---
+++ chat_messenger/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'pages/loading_screen.dart';
+import 'pages/error_screen.dart';
+import 'pages/auth_checker.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+//  This is a FutureProvider that will be used to check whether the firebase has been initialized or not
+final firebaseinitializerProvider = FutureProvider<FirebaseApp>((ref) async {
+  return await Firebase.initializeApp();
+});
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //  We will watch this provider to see if the firebase has been initialized
+    //  As said this gives async value so it can gives 3 types of results
+    //  1. The result is a Future<FirebaseApp>
+    //  2. The result is a Future<Error>
+    //  3. It's still loading
+
+    final initialize = ref.watch(firebaseinitializerProvider);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      //  We will use the initialize to check if the firebase has been initialized
+      //  .when function can only be used with AsysncValue. If you hover over intialize
+      //  you can see what type of variable it is. I have left it dynamic here for your better understanding
+      //  Though it's always recommended to not to use dynamic variables.
+
+      // Now here if the Firebase is initialized we will be redirected to AuthChecker
+      // which checks if the user is logged in or not.
+
+      //  the other Two functions speaks for themselves.
+      home: initialize.when(
+          data: (data) {
+            return const AuthChecker();
+          },
+          loading: () => const LoadingScreen(),
+          error: (e, stackTrace) => ErrorScreen(e, stackTrace)),
+    );
+  }
+}
---
+++ chat_messenger/lib/providers/auth_provider.dart
@@ -0,0 +1,38 @@
+import 'package:chat_messenger/models/auth_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//  This is how you create a provider in Riverpod. Note the syntax may change in near future.
+//  This is a provider which provides all the features of Authentication class we have created
+
+//  The syntax is pretty simple.
+//  you are using a Class Provider and specifiying the type of provider.
+//  now this takes a function takes a providerreference ref as a parameter
+//  this ref can you used to access a provider within a provider.
+//  if you are not using a provider within a provd`ier, no worries. It's not compulosry.
+//  you can use a provider without a provider.
+
+final authenticationProvider = Provider<Authentication>((ref) {
+  return Authentication();
+});
+
+//  Here I have shared the example of a provider used within a provider.
+// keep in mind I am reading a provider from a provider not watching it.
+//  The docs mention not to use watch in a provider. This is bad for performance
+//  if the data changes conitnously your app will suck bad
+
+final authStateProvider = StreamProvider<User?>((ref) {
+  return ref.read(authenticationProvider).authStateChange;
+});
+
+//  There are different Types of Provider
+//  Provider<T> is the most basic type of provider
+//  FutureProvider<T> which involes a Future
+//  StreamProvider<T> which involves a Stream
+//  and many more. Refer to their docs for more info
+
+//  Creating a firebaseAuthProvider to get some basic details of the loggedIn user
+//  though we can store it in da`tabase but for now we will just use it to get the user
+final fireBaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
